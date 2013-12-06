55dbf4ec9c44f00b19ae0c47129756a4708e5e58
3vi1john <jbabio@me.com>
2013-12-06 10:25:06 -0500

    Added bro install script

---
+++ bro-install-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get -y install wget build-essential cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev
+
+wget -O/tmp/bro-2.2.tar.gz http://www.bro.org/downloads/release/bro-2.2.tar.gz 
+
+cd /tmp
+tar -xvf bro-2.2.tar.gz
+cd /tmp/bro-2.2
+sudo ./configure && sudo make && sudo make install
+
+
+sudo sed 's/interface=eth0/interface=eth1/g' /usr/local/bro/etc/node.cfg > /usr/local/bro/etc/node1
+sudo mv /usr/local/bro/etc/node1 /usr/local/bro/etc/node.cfg
+
+sudo ifconfig eth1 0.0.0.0 up promisc
+
+sudo /usr/local/bro/bin/broctl install
+sudo /usr/local/bro/bin/broctl start
+
+
