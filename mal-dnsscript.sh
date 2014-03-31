cdf07c83bcef7b423b31c3bee3d7479a0d5e8f34
3vi1john <jbabio@me.com>
2014-03-31 14:23:57 -0400

    Update mal-dnsscript.sh
    Removes duplicates now.

--- mal-dnsscript.sh
+++ mal-dnsscript.sh
@@ -20,24 +20,47 @@
 ##
 
 #This creates the dns intel files
-/usr/bin/mal-dnssearch -M mandiant -p | mal-dns2bro -T dns -s mandiant -n true > baddns
-/usr/bin/mal-dnssearch -M mayhemic -p | mal-dns2bro -T dns -s mayhemic -n true >> baddns
-/usr/bin/mal-dnssearch -M malhosts -p | mal-dns2bro -T dns -s malwaredomainlist -n true >> baddns
+/usr/bin/mal-dnssearch -M mandiant 
+/usr/bin/mal-dnssearch -M mayhemic 
+/usr/bin/mal-dnssearch -M malhosts
+cat mandiant_apt1.dns > dnsnames.txt
+cat malhosts.txt >> dnsnames.txt
+cat hosts.txt >> dnsnames.txt
+/usr/bin/awk ' !x[$0]++' dnsnames.txt > baddns
 
-#This removes the duplicate #field header section of intel files 
-/usr/bin/awk ' !x[$0]++' baddns > baddns.intel
+cat baddns | mal-dns2bro -T dns -s malware-domain-names -n true > baddns.intel
+mv baddns.intel /usr/local/bro/share/bro/inteldata
+rm mandiant_apt1.dns
+rm malhosts.txt
+rm hosts.txt
 rm baddns
+rm dnsnames.txt
+
 
 #This creates the ip intel files
-/usr/bin/mal-dnssearch -M snort -p | mal-dns2bro -T ip -s snort -n true > badips
-/usr/bin/mal-dnssearch -M et_ips -p | mal-dns2bro -T ip -s emergingthreats -n true >> badips
-/usr/bin/mal-dnssearch -M botcc -p | mal-dns2bro -T ip -s emergingthreats -n true >> badips
-/usr/bin/mal-dnssearch -M tor -p | mal-dns2bro -T ip -s emergingthreats -n true >> badips
-/usr/bin/mal-dnssearch -M rbn -p | mal-dns2bro -T ip -s emergingthreats -n true >> badips
-/usr/bin/mal-dnssearch -M malips -p | mal-dns2bro -T ip -s malwaredomainlist -n true >> badips
-/usr/bin/mal-dnssearch -M ciarmy -p | mal-dns2bro -T ip -s ciarmy -n true >> badips
-/usr/bin/awk ' !x[$0]++' badips > badips.intel
+/usr/bin/mal-dnssearch -M snort
+/usr/bin/mal-dnssearch -M et_ips
+/usr/bin/mal-dnssearch -M botcc
+/usr/bin/mal-dnssearch -M tor
+/usr/bin/mal-dnssearch -M malips
+/usr/bin/mal-dnssearch -M ciarmy
+cat ip.txt > ipaddr.txt
+cat compromised-ips.txt >> ipaddr.txt
+cat ci-badguys.txt >> ipaddr.txt
+cat botcc.rules >> ipaddr.txt
+cat tor.rules >> ipaddr.txt
+cat ip-filter.blf >> ipaddr.txt
+/usr/bin/awk ' !x[$0]++' ipaddr.txt > badips
+cat badips | mal-dns2bro -T ip -s malware-ips -n true > badips.intel
+mv badips.intel /usr/local/bro/share/bro/inteldata
+rm ipaddr.txt
 rm badips
+rm ip.txt
+rm compromised-ips.txt
+rm ci-badguys.txt
+rm botcc.rules
+rm tor.rules
+rm ip-filter.blf
 
 mv baddns.intel /usr/local/bro/share/bro/inteldata
 mv badips.intel /usr/local/bro/share/bro/inteldata
