efaab57f9f84aad820ed9e90a647854954ffc44d
3vi1john <jbabio@me.com>
2015-12-27 22:19:12 -0500

    Update bro-install-script.sh

--- bro-install-script.sh
+++ bro-install-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get -y install wget build-essential cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev
 
-wget -O/tmp/bro-2.4.tar.gz https://www.bro.org/downloads/release/bro-2.4.tar.gz
+wget -O/tmp/bro-2.4.1.tar.gz https://www.bro.org/downloads/release/bro-2.4.1.tar.gz
 
 cd /tmp
-tar -xvf bro-2.4.tar.gz
-cd /tmp/bro-2.4
+tar -xvf bro-2.4.1.tar.gz
+cd /tmp/bro-2.4.1
 sudo ./configure && sudo make && sudo make install
 
 sudo sed -i 's/interface=eth0/interface=eth1/g' /usr/local/bro/etc/node.cfg 
